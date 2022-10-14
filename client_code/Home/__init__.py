0b315a7a3816acc6ead45afb58bf70b46e11af8f
ottoa98@yahoo.com <ottoa98@yahoo.com>
2022-10-14 21:47:00 +0000

    Edited otto_and_associates_logo.png, settings and Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -11,6 +11,8 @@ class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.items = app_tables.pricing.search()
+    
 
     # Any code you write here will run when the form opens.
+
+
