36a99651887b88cef67fe834efe53b321ddeb633
ottoa98@yahoo.com <ottoa98@yahoo.com>
2022-10-19 15:35:14 +0000

    Created Agent_Appointments.RowTemplate3 and Agent_Appointments.RowTemplate2; Edited Agent_Appointments

--- client_code/Agent_Appointments/__init__.py
+++ client_code/Agent_Appointments/__init__.py
@@ -10,8 +10,17 @@ class Agent_Appointments(Agent_AppointmentsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.sort_drop.items = [("Sort by date", "datetime"), ("Sort by user", "user")]
+    current_bookings, past_bookings = anvil.server.call('get_bookings')
+    self.repeating_panel_1.items = current_bookings
+    self.repeating_panel_2.items = past_bookings
 
-    # Any code you write here will run when the form opens.
+
+  def sort_drop_change(self, **event_args):
+    """This method is called when an item is selected"""
+    current_bookings, past_bookings = anvil.server.call('get_bookings', self.sort_drop.selected_value)
+    self.repeating_panel_1.items = current_bookings
+    self.repeating_panel_2.items = past_bookings
 
   def appointments_link_click(self, **event_args):
     """This method is called when the link is clicked"""
