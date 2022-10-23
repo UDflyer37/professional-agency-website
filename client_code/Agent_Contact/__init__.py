f4cb32fae9aefbc7cc509d673e8bbab0df183300
ottoa98@yahoo.com <ottoa98@yahoo.com>
2022-10-23 20:55:39 +0000

    Edited Agent_Contact

--- client_code/Agent_Contact/__init__.py
+++ client_code/Agent_Contact/__init__.py
@@ -17,7 +17,7 @@ class Agent_Contact(Agent_ContactTemplate):
     """This method is called when the button is clicked"""
     email = self.user_email.text
     body = self.message_area.text
-    subject = "A Message About Your Appointment"
+    subject = "A Message From Otto & Associates"
     attachments = self.file_loader_1.files
     if body:
       anvil.server.call('send_email', email, body, subject, attachments)
