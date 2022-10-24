60716eee6dd470e7edb494818e5fd7863597e367
ottoa98@yahoo.com <ottoa98@yahoo.com>
2022-10-24 01:54:16 +0000

    Created Agent_Resources.ItemTemplate5; Edited settings, Agent_Resources.ItemTemplate4, Agent_Resources and Home

--- client_code/Agent_Resources/__init__.py
+++ client_code/Agent_Resources/__init__.py
@@ -11,6 +11,27 @@ class Agent_Resources(Agent_ResourcesTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    self.repeating_panel_1=app_tables.web_resources.get(title=title,URL=URL)
+    app_tables.client_resources.client_readable()
+    self.repeating_panel_client_resources.items=app_tables.client_resources.search(tables.order_by("main title"))
+    
+    app_tables.agent_resources.client_readable()
+    self.repeating_panel_agent_resources.items=app_tables.agent_resources.search(tables.order_by("main title"))
 
+    
 #    app_tables.bookings.add_row(name=name.capitalize().strip(), datetime=date, user=email.lower().strip())
+
+  def client_resources_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    if self.client_resources_card.visible==False:
+      self.client_resources_card.visible=True
+    else:
+      self.client_resources_card.visible=False
+
+  def agent_resources_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    if self.agent_resources_card.visible==False:
+      self.agent_resources_card.visible=True
+    else:
+      self.agent_resources_card.visible=False
+
+
