37fa6502439793b9c07b0d6ad388031db16c7f2a
ottoa98@yahoo.com <ottoa98@yahoo.com>
2022-11-23 19:46:30 +0000

    Edited Appointment, Contact.email_us, theme.css, Contact, start-img.jpg, Contact.contact_us and Home

--- client_code/Contact/__init__.py
+++ client_code/Contact/__init__.py
@@ -17,19 +17,11 @@ class Contact(ContactTemplate):
   def contact_link_click(self, **event_args):
     """This method is called when the link is clicked"""
     alert(contact_us(item=self.item), large=True, buttons=[("Close", False)])
-    if self.contact_card.visible:
-        self.contact_card.visible = False
-    else:
-        self.contact_card.visible = True
+
 
   def email_link_click(self, **event_args):
     """This method is called when the link is clicked"""
-    alert(email_us(item=self.item), large=True, buttons=[("Submit", True), ("Close", False)])
-    if self.email_card.visible:
-        self.email_card.visible = False
-        self.email_template_card.visible = False
-    else:
-        self.email_card.visible = True
+    value = alert(email_us(item=self.item), large=True, buttons=[("Close", False)])
 
 
 
