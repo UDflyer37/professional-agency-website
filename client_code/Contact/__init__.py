851ac41b86ccb739065a9f786cde2302245e5a12
ottoa98@yahoo.com <ottoa98@yahoo.com>
2022-11-23 16:58:20 +0000

    Edited Contact.email_us, Contact and Contact.contact_us

--- client_code/Contact/__init__.py
+++ client_code/Contact/__init__.py
@@ -5,7 +5,8 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from . import 
+from .contact_us import contact_us
+from .email_us import email_us
 
 
 class Contact(ContactTemplate):
@@ -13,75 +14,9 @@ class Contact(ContactTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Set up google map
-    map = self.map_1
-    
-    map.center = GoogleMap.LatLng(39.8405849898327, -84.1375283738747)
-    map.zoom = 15
-    
-    marker = GoogleMap.Marker(
-      animation=GoogleMap.Animation.DROP,
-      position=GoogleMap.LatLng(39.8405849898327, -84.1375283738747)
-    )
-    map.add_component(marker)
-    
-    #TODO: put items in designer
-    self.topic_drop.items = ['Quote', 'Payment', 'Feedback', 'Other']
-
-  def submit_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    name = self.name_box.text
-    user = self.email_box.text
-    topic = self.topic_drop.selected_value
-    question = self.question_area.text
-    if name and user and topic and question:
-      anvil.server.call('add_contact_info', name, user, topic, question)
-      alert("Your question/comment has been submitted!")
-      self.name_box.text = ""
-      self.email_box.text = ""
-      self.topic_drop.selected_value = None
-      self.question_area.text = ""
-    else:
-      alert("Please fill out the entire form before submitting.")
-
-  def proof_of_insurance_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.topic_drop.items = ['Request a Proof of Insurance Card']
-    self.topic_drop.selected_value = "Request a Proof of Insurance Card"
-    self.question_area.text = "I would like to Request my Proof of Insurance Card."
-    self.email_template_card.visible = True
-
-  def declaration_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.topic_drop.items = ['Request a Declaration']
-    self.topic_drop.selected_value = "Request a Declaration"
-    self.question_area.text = "I would like to Request a Declaration."
-    self.email_template_card.visible = True
-
-  def certificate_of_insurance_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.topic_drop.items = ['Request a Certificate of Insurance']
-    self.topic_drop.selected_value = "Request a Certificate of Insurance"
-    self.question_area.text = "I would like to Request a Certificate of Insurance."
-    self.email_template_card.visible = True
-
-  def insurance_verification_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.topic_drop.items = ['Request Verification of Insurance']
-    self.topic_drop.selected_value = "Request Verification of Insurance"
-    self.question_area.text = "I would like to Request Verification of Insurance."
-    self.email_template_card.visible = True
-
-  def other_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.topic_drop.items = ['Quote', 'Payment', 'Feedback', 'Other']
-    self.topic_drop.focus()
-    self.question_area.text = ""
-    self.email_template_card.visible = True
-
   def contact_link_click(self, **event_args):
     """This method is called when the link is clicked"""
-    alert(self.contact_us(item=self.item), large=True, buttons=[("Submit", True), ("Close", False)])
+    alert(contact_us(item=self.item), large=True, buttons=[("Close", False)])
     if self.contact_card.visible:
         self.contact_card.visible = False
     else:
@@ -89,7 +24,7 @@ class Contact(ContactTemplate):
 
   def email_link_click(self, **event_args):
     """This method is called when the link is clicked"""
-    
+    alert(email_us(item=self.item), large=True, buttons=[("Submit", True), ("Close", False)])
     if self.email_card.visible:
         self.email_card.visible = False
         self.email_template_card.visible = False
