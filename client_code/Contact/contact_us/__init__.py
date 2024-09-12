2be54f2a9df0199997f24801be61fd7a50eb39e5
ottoa98@yahoo.com <ottoa98@yahoo.com>
2024-09-12 18:13:29 +0000

    Created location_front.png and location.png; Edited Contact.contact_us

--- client_code/Contact/contact_us/__init__.py
+++ client_code/Contact/contact_us/__init__.py
@@ -14,31 +14,7 @@ class contact_us(contact_usTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Set up google map
-    map = self.map_1
     
-    map.center = GoogleMap.LatLng(39.8405849898327, -84.1375283738747)
-    map.zoom = 15
-    
-    marker = GoogleMap.Marker(
-      animation=GoogleMap.Animation.DROP,
-      position=GoogleMap.LatLng(39.8405849898327, -84.1375283738747)
-    )
-    map.add_component(marker)
-
-  def map_1_show(self, **event_args):
-    """This method is called when the GoogleMap is shown on the screen"""
-        # Set up google map
-    map = self.map_1
-    
-    map.center = GoogleMap.LatLng(39.8405849898327, -84.1375283738747)
-    map.zoom = 15
-    
-    marker = GoogleMap.Marker(
-      animation=GoogleMap.Animation.DROP,
-      position=GoogleMap.LatLng(39.8405849898327, -84.1375283738747)
-    )
-    map.add_component(marker)
 
 
     
